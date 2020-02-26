8aaa8405caa5febaf1b8efc5ae0a2f72cc43a1b2
Michal Matous <michal.matous@gmail.com>
2020-02-26 02:35:10 +0100

    #5 - clock, periodic task

--- services/api/app/lib/model.py
+++ services/api/app/lib/model.py
@@ -5,7 +5,7 @@ from tortoise import fields
 class Products(Model):
 
     id = fields.UUIDField(pk=True)
-    name = fields.CharField(255, unique=True)
+    name = fields.CharField(255)
     description = fields.TextField()
 
     def __str__(self):
@@ -21,13 +21,13 @@ class Products(Model):
 
 class Offers(Model):
 
-    id = fields.UUIDField(pk=True)
+    id = fields.CharField(255, pk=True)
 
     product = fields.ForeignKeyField(
         'model.Products', related_name='offers')
 
-    price = fields.IntField()
-    items_in_stock = fields.IntField()
+    price = fields.IntField(null=True)
+    items_in_stock = fields.IntField(null=True)
 
     def __str__(self):
         return self.id
